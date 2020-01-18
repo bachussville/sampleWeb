187f0aa3a3e90268cae9325bc4cb4d431be8b3d7
Bart <Bart@192.168.0.148>
2020-01-18 13:04:12 +0100

    Additional tests: DBManager.saveTreeClass

---
+++ src/main/java/com/bville/sampleproj/config/BConfig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bville.sampleproj.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Bart
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.bville.bachussweb"})
+public class BConfig {
+    
+}
